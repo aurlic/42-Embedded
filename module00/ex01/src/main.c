1b6fbc5de970ef1fec0fdc44480afdaccc05c7ec
Arthur Urlic <aurlic@student.42.fr>
2025-03-03 14:16:00 +0100

    chore: format comments

--- module00/ex01/src/main.c
+++ module00/ex01/src/main.c
@@ -3,22 +3,19 @@
 int main(void) {
   /*
   DDRB is the Data Direction Register for port B.
-  Each bit in this register corresponds to a pin on port B.
-  Setting a bit to 1 configures the corresponding pin as an output.
-  (1 << DDB0) shifts the bit 1 to the DDB0 position, which corresponds to pin
-  PB0. The |= operator performs a bitwise OR operation, allowing PB0 to be set
-  as an output without affecting other pins.
+  (1 << DDB0) shifts the bit 1 to the DDB0 position, and sets it as an output.
+
+  (cf. datasheet 14.2, 14.4)
   */
   DDRB |= (1 << DDB0);
 
   /*
   PORTB is the Output Register for port B.
-  Setting a bit to 1 in this register sets the corresponding pin to the HIGH
-  state (5V). (1 << PORTB0) shifts the bit 1 to the PORTB0 position, which
-  corresponds to pin PB0. The |= operator performs a bitwise OR operation,
-  allowing PB0 to be set to HIGH without affecting other pins.
+  (1 << PORTB0) shifts the bit 1 to the PORTB0 position, and sets it to HIGH.
+
+  (cf. datasheet 14.4)
   */
-  PORTB |= (1 << PB0);
+  PORTB |= (1 << PORTB0);
 
   return 0;
 }
