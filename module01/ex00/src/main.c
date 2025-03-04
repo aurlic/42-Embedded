b732183122b2f8d5d6c4def4ae78ae1a6679d28b
Arthur Urlic <aurlic@student.42.fr>
2025-03-04 09:48:38 +0100

    feat: blink led every second (using volatile works)

---
+++ module01/ex00/src/main.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+
+void delay_ms(uint16_t ms) {
+  for (volatile uint16_t i = 0; i < ms; i++) {
+    // Wait for about 1ms
+    for (volatile uint16_t j = 0; j < 1600; j++) {
+    }
+  }
+}
+
+int main(void) {
+  // Set PB1 as an output
+  DDRB |= (1 << DDB1);
+
+  while (1) {
+    // Invert LED state
+    PORTB ^= (1 << PORTB1);
+
+    // Wait for about 500ms
+    delay_ms(500);
+  }
+}
